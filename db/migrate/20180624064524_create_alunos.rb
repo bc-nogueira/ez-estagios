f02b4f2b3a4780321699960305e5507b8d236c06
Prikster <brenocnog@gmail.com>
2018-06-24 03:48:44 -0300

    Corrigindo migration e rodando migrate.

--- db/migrate/20180624064524_create_alunos.rb
+++ db/migrate/20180624064524_create_alunos.rb
@@ -2,7 +2,7 @@ class CreateAlunos < ActiveRecord::Migration[5.2]
   def change
     create_table :alunos do |t|
       t.string :matricula
-      t.number :periodo
+      t.integer :periodo
       t.boolean :validado
 
       t.timestamps
