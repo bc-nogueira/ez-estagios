c56bb08e0aa472b9f5fa550aa1101894d932009c
Breno Nogueira <bcnogueira@id.uff.br>
2018-06-25 10:28:56 -0300

    Adicionando criação de coordenador no seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
+# Cria uma empresa nova, com sua conta
 empresa = Empresa.new({ nome: 'STI', cnpj: '111111', endereco: 'Rua Um' })
 Account.create({ email: 'sti@gmail.com', password: '123456', perfil: empresa })
+
+# Cria um coordenador com sua conta
+coord = Coordenador.new(curso: 'Sistemas de Informação')
+pessoa = Pessoa.new({ nome: 'Breno', cpf: '11111111111', papel: coord })
+Account.create({ email: 'breno@gmail.com', password: '123456', perfil: pessoa })
