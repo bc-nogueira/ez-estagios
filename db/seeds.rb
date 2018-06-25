b2d7e8dae816d1bf12ff625be5a726a3e9ae3443
Breno Nogueira <bcnogueira@id.uff.br>
2018-06-25 11:37:25 -0300

    Atualizando seed com mais empresas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 # Cria uma empresa nova, com sua conta
-empresa = Empresa.new({ nome: 'STI', cnpj: '111111', endereco: 'Rua Um' })
-Account.create({ email: 'sti@gmail.com', password: '123456', perfil: empresa })
+sti = Empresa.new({ nome: 'STI', cnpj: '111111', endereco: 'Valonguinho' })
+Account.create({ email: 'sti@gmail.com', password: '123456', perfil: sti })
+
+uff = Empresa.new({ nome: 'STI', cnpj: '222222', endereco: 'Reitoria', validada: true })
+Account.create({ email: 'uff@gmail.com', password: '123456', perfil: uff })
+
+guanabara = Empresa.new({ nome: 'Guanabara', cnpj: '333333', endereco: 'Niteroi', validada: false })
+Account.create({ email: 'guanabara@gmail.com', password: '123456', perfil: guanabara })
 
 # Cria um coordenador com sua conta
 coord = Coordenador.new(curso: 'Sistemas de Informação')
