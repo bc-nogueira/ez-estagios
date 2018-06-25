d1486c320bf370f4916a81dcfa327662d3246bef
Breno Nogueira <bcnogueira@id.uff.br>
2018-06-25 01:14:24 -0300

    Adicionando null false para empresa

--- db/migrate/20180624064054_create_empresas.rb
+++ db/migrate/20180624064054_create_empresas.rb
@@ -1,10 +1,10 @@
 class CreateEmpresas < ActiveRecord::Migration[5.2]
   def change
     create_table :empresas do |t|
-      t.string :nome
-      t.string :cnpj
-      t.string :endereco
-      t.boolean :validada
+      t.string :nome, null: false
+      t.string :cnpj, null: false
+      t.string :endereco, null: false
+      t.boolean :validada, null: false, default: false
 
       t.timestamps
     end
