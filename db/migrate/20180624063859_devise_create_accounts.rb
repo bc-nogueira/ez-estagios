20d31f0afb1db237c04ceeaecf71aebaff12f4e8
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2018-07-07 19:42:28 -0300

    Correções do rubocop

--- db/migrate/20180624063859_devise_create_accounts.rb
+++ db/migrate/20180624063859_devise_create_accounts.rb
@@ -4,8 +4,8 @@ class DeviseCreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
       t.references :perfil, polymorphic: true, index: true
 
       ## Recoverable
