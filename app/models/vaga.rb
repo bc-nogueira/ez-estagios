5b273d5893450caddcae191d7979ae2aec597888
Romulo Mourão <romulo.dam@gmail.com>
2018-07-08 22:17:03 -0300

    Adiciona delegates em Vaga

--- app/models/vaga.rb
+++ app/models/vaga.rb
@@ -3,4 +3,7 @@ class Vaga < ApplicationRecord
 
   scope :por_empresa, ->(id) { where(empresa_id: id) }
   scope :validadas, -> { where(validada: true) }
+
+  delegate :nome, to: :empresa, prefix: true
+  delegate :avatar, to: :empresa, prefix: true
 end
