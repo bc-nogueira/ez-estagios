c39497677c61bdd644b1990c99ab5eae551c1645
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2018-07-10 01:15:37 -0300

    Alterando scope de vaga validada e config do rubocop

--- app/models/vaga.rb
+++ app/models/vaga.rb
@@ -3,7 +3,9 @@ class Vaga < ApplicationRecord
   has_many :vaga_habilidades
 
   scope :por_empresa, ->(id) { where(empresa_id: id) }
-  scope :validadas, -> { where(validada: true) }
+  scope :validadas, -> do
+    joins(:empresa).where(validada: true, empresas: { validada: true })
+  end
 
   delegate :nome, to: :empresa, prefix: true
   delegate :avatar, to: :empresa, prefix: true
