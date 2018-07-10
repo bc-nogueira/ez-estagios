e6d3771218d62c607036e0d1790194e57f7e868a
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2018-07-10 00:22:48 -0300

    Finalizando avaliação da empresa, melhorando tela de candidatura para aluno, consertando situação de candidatura

--- app/helpers/aluno_vagas_helper.rb
+++ app/helpers/aluno_vagas_helper.rb
@@ -2,7 +2,8 @@ module AlunoVagasHelper
   def texto_situacao_aluno_vaga(aluno_vaga)
     return 'Liberação Pendente' if aluno_vaga.validado_coordenador.nil?
     return 'Rejeitado Coordenador' unless aluno_vaga.validado_coordenador
-    return 'Aprovação Pendente' unless aluno_vaga.validado_empresa
+    return 'Aprovação Pendente' if aluno_vaga.validado_empresa.nil?
+    return 'Rejeitado' unless aluno_vaga.validado_empresa
     'Aprovado'
   end
 end
