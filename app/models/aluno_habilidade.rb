124aa4e9c7b3e9cebeb1d40c9ca127c382aad10d
Romulo Mourão <romulo.dam@gmail.com>
2018-07-10 18:05:22 -0300

    Adiciona Habilidades para Aluno

--- app/models/aluno_habilidade.rb
+++ app/models/aluno_habilidade.rb
@@ -1,4 +1,8 @@
 class AlunoHabilidade < ApplicationRecord
   belongs_to :aluno
   belongs_to :habilidade
+
+  delegate :nome, to: :habilidade
+
+  enum nivel: { basico: 0, intermediario: 1, avancado: 2 }
 end
