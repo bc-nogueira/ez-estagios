3dbc87f1d50c990b9914bdf57edb7f6fe77bee54
Breno Nogueira <bcnogueira@id.uff.br>
2018-07-11 15:38:03 -0300

    Pequenas correções

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  devise_scope :account do
-    root to: 'devise/sessions#new'
+  devise_for :accounts, controllers: { registrations: 'accounts/registrations' }
+
+  authenticated :account do
+    root to: 'home#index', as: :authenticated_root
   end
+
+  root to: redirect('/accounts/sign_in')
 
   # Páginas que precisam de login para serem acessadas
   authenticate :account do
     get 'index', to: 'home#index'
 
-    resources :aluno_horarios
+    # TODO: Implementar aluno horários
+    # resources :aluno_horarios
     resources :aluno_habilidades, only: [:create, :destroy]
     resources :vaga_habilidades, only: [:create, :destroy]
     resources :aluno_vagas, only: [:index, :create, :update, :destroy]
@@ -27,6 +32,4 @@ Rails.application.routes.draw do
 
     resources :instrucao, only: :index
   end
-
-  devise_for :accounts, controllers: { registrations: 'accounts/registrations' }
 end
