d88c47ad3fe5ed643970203feb992b60bb623cc4
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2018-07-11 01:38:10 -0300

    Consertando scopes e adicionando association

--- app/models/habilidade.rb
+++ app/models/habilidade.rb
@@ -1,2 +1,4 @@
 class Habilidade < ApplicationRecord
+  has_many :aluno_habilidades
+  has_many :vaga_habilidades
 end
--- app/models/vaga.rb
+++ app/models/vaga.rb
@@ -2,15 +2,16 @@ class Vaga < ApplicationRecord
   belongs_to :empresa
   has_many :vaga_habilidades
   has_many :aluno_vagas
+  has_many :habilidades, through: :vaga_habilidades
   delegate :nome, to: :empresa, prefix: true
   delegate :avatar, to: :empresa, prefix: true
 
   scope :por_empresa, ->(id) { where(empresa_id: id) }
   scope :match_habilidades, ->(aluno) {
-    joins(:vaga_habilidades)
-    .joins("LEFT JOIN aluno_habilidades on vaga_habilidades.habilidade_id = aluno_habilidades.habilidade_id")
-    .where(aluno_habilidades: {aluno_id: aluno})
+    joins(habilidades: :aluno_habilidades)
+    .where(aluno_habilidades: { aluno_id: aluno })
   }
+
   scope :validadas, -> do
     joins(:empresa).where(validada: true, empresas: { validada: true })
   end
